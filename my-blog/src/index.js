bf2279642506ea0ae71ae94396e2e657394fbbec
Cristian Keisz <cristian.keisz@tss-yonder.com>
2024-06-04 23:35:30 +0300

    add firebase login page

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdjtbYftQlQ7vF7kAun2_k7u0gZQ0Pu7g",
+  authDomain: "my-react-blog-a0a33.firebaseapp.com",
+  projectId: "my-react-blog-a0a33",
+  storageBucket: "my-react-blog-a0a33.appspot.com",
+  messagingSenderId: "967574909709",
+  appId: "1:967574909709:web:cdc95e1c3726281fbff644"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ my-blog/src/pages/LogInPage.js
@@ -0,0 +1,46 @@
+import {React, useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+
+const LogInPage = () => {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [error,setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const logIn= async () => {
+        try{
+        await signInWithEmailAndPassword(getAuth(), email,password);
+        navigate('/articles');
+        }catch (e){
+            setError(e.message)
+        }
+    }
+   
+  return (
+    <div id="add-comment-form">
+        <h1>Log In</h1>
+        {error && <p className='error'>{error}</p>}
+        <label>
+            Email:
+            <input
+            placeholder='Your Email Address'
+            value={email}
+            onChange={e=>setEmail(e.target.value)}
+             type='text' />
+        </label>
+        <label>
+            Password:
+            <input
+            type='password'
+            value={password}
+            onChange={e=>setPassword(e.target.value)} />
+        </label>
+        <button onClick={logIn}>Log In</button>
+        <Link to="/createAccount">Don't have account. Create one here</Link>
+    </div>
+  )
+}
+
+export default LogInPage
